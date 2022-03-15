5bc800f9f1a8df4ccf1ce0d0fb184448dc4b930f
JMSCarrascal <juansierracarrascal@gmail.com>
2022-03-15 09:29:59 -0500

    add model,view and controler to User

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+   
+    public function index()
+    {
+        return view('admin.users.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return ("Aqui puedes crear un usario");
+    }
+
+    
+    public function store(Request $request)
+    {
+        return ("Aqui puedes almacenar un usario");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+use App\Models\User;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [UsersController::class, 'index'])->name('admin.users.index');
+Route::get('/create', [UsersController::class, 'create'])->name('admin.users.create');
+Route::get('/store', [UsersController::class, 'store'])->name('admin.users.store');
+// Route::get('/{user}/show', [UsersController::class, 'show'])->name('admin.users.show');
+// Route::put('/{user}', [UsersController::class, 'update'])->name('admin.users.update');
+// Route::get('/{user}/destroy', [UsersController::class, 'destroy'])->name('admin.users.destroy');
+// Route::get('/{userId}/restore', [UsersController::class, 'restore'])->name('admin.users.restore');
+// Route::put('/{user}/change-password', [UsersController::class, 'changePassword'])->name('admin.users.changePassword'
+
+
